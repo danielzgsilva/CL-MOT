340990f5dfb6776ef889075866f0f3be9f0307e4
Daniel Silva <danielzgsilva@knights.ucf.edu>
2020-07-24 01:39:46 -0400

    count

--- src/list_panda_files.py
+++ src/list_panda_files.py
@@ -9,6 +9,7 @@ seqs = [s for s in os.listdir(seq_root)]
 
 output_file = 'panda.train'
 
+i = 0
 for seq in seqs:
     fnames = os.listdir(os.path.join(seq_root, seq))
     seq_info = json.load(open(os.path.join(seq_root, seq, 'seqinfo.json')))
@@ -18,4 +19,8 @@ for seq in seqs:
         file_str = os.path.join(panda_root, seq, fname)
 
         if fname.endswith(img_ext):
+            i += 1
             print(file_str)
+
+
+print(i)
